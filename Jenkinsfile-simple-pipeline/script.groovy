13e3bfba8d9f8bddc8ac4e7879b9bd03e6704331
Denis <denis@example.com>
2024-09-30 17:02:49 +0300

    groovy changes

--- Jenkinsfile-simple-pipeline/script.groovy
+++ Jenkinsfile-simple-pipeline/script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cr', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t denchikkarate/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push denchikkarate/demo-app:jma-2.0'
     }
 } 
 
