67deb40c7edca60a1c678ca006e206dabf4af2ea
Denis <denis@example.com>
2024-10-08 17:01:04 +0300

    fix Jenkinsfile

--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
 def buildJar() {
-    echo "building the application..."
+    echo "building the application v2 ..."
     sh 'mvn package'
 } 
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    echo "building the docker image v2..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cr', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t denchikkarate/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push denchikkarate/demo-app:jma-2.0'
     }
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo 'deploying the application v2 ...'
 } 
 
 return this
